85a53176fd3043e985eaf3ddd6f0c8d50743503e
Mriganka Dhar <mrigankodhar0001@gmail.com>
2024-05-24 09:08:50 +0530

    Added User model

---
+++ api/user.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/MRD1920/Gobank.git/util"
+	"github.com/gin-gonic/gin"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+type createUserResponse struct {
+	Username          string `json:"username"`
+	FullName          string `json:"full_name"`
+	Email             string `json:"email"`
+	PasswordChangedAt string `json:"password_changed_at"`
+	CreatedAt         string `json:"created_at"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	//TODO: create user param.
+	args := db.create
+}
