86cd333a6951af6bddfeb07ad0f2fb0f316de345
Mriganka Dhar <mrigankodhar0001@gmail.com>
2024-08-17 08:45:43 +0530

    Changed service name.

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net"
 
 	"github.com/MRD1920/Gobank.git/api"
 	db "github.com/MRD1920/Gobank.git/db/sqlc"
+	"github.com/MRD1920/Gobank.git/gapi"
+	"github.com/MRD1920/Gobank.git/pb"
 	"github.com/MRD1920/Gobank.git/util"
 	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -23,13 +28,42 @@ func main() {
 	}
 	// Create a new store
 	store := db.NewStore(conn)
-	server,err := api.NewServer(config,store);
 
+	//run the Gin server
+	// runGinServer(config, store)
+	runGrpcServer(config, store)
+
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
 	if err != nil {
-		log.Fatal("cannot create server:",err);
+		log.Fatal("cannot create a grpc server: ", err)
 	}
 
-	err = server.Start(config.ServerAddress)
+	grpcServer := grpc.NewServer()
+	pb.RegisterGoBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server: ", err)
+	}
+	log.Printf("start gRPC server at %s", config.GRPCServerAddress)
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPC server:", err)
+	}
+}
+
+func runGinServer(config util.Config, store db.Store) {
+	server, err := api.NewServer(config, store)
+
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	err = server.Start(config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("Cannot start server:", err)
 	}
